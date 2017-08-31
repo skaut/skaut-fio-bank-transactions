588362d060a4fa5fc0fa5681073410a0cf9a7a93
Michal Janata <mjanata@gmail.com>
2017-08-31 10:23:44 +0200

    úpravy překladů

--- src/accounts/AccountsInit.php
+++ src/accounts/AccountsInit.php
@@ -31,33 +31,33 @@ final class AccountsInit {
 
 	public function registerPostType() {
 		$labels       = [
-			'name'                  => _x( 'Správa účtů', 'Post Type General Name', 'fio-transactions' ),
-			'singular_name'         => _x( 'Účet', 'Post Type Singular Name', 'fio-transactions' ),
-			'menu_name'             => __( 'Správa účtů', 'fio-transactions' ),
-			'name_admin_bar'        => __( 'Správa účtů', 'fio-transactions' ),
-			'archives'              => __( 'Archiv účtů', 'fio-transactions' ),
-			'attributes'            => __( 'Atributy', 'fio-transactions' ),
-			'parent_item_colon'     => __( 'Nadřazený účet', 'fio-transactions' ),
-			'all_items'             => __( 'Správa účtů', 'fio-transactions' ),
-			'add_new_item'          => __( 'Přidat nový účet', 'fio-transactions' ),
-			'add_new'               => __( 'Přidat účet', 'fio-transactions' ),
-			'new_item'              => __( 'Nový účet', 'fio-transactions' ),
-			'edit_item'             => __( 'Upravit účet', 'fio-transactions' ),
-			'update_item'           => __( 'Aktualizovat účet', 'fio-transactions' ),
-			'view_item'             => __( 'Zobrazit účet', 'fio-transactions' ),
-			'view_items'            => __( 'Zobrazit účet', 'fio-transactions' ),
-			'search_items'          => __( 'Hledat v účtech', 'fio-transactions' ),
-			'not_found'             => __( 'Žádné účty', 'fio-transactions' ),
-			'not_found_in_trash'    => __( 'Koš je prázdný', 'fio-transactions' ),
-			'featured_image'        => __( 'Náhledový obrázek', 'fio-transactions' ),
-			'set_featured_image'    => __( 'Zadat náhledový obrázek', 'fio-transactions' ),
-			'remove_featured_image' => __( 'Odstranit náhledový obrázek', 'fio-transactions' ),
-			'use_featured_image'    => __( 'Použít jako náhledový obrázek', 'fio-transactions' ),
-			'insert_into_item'      => __( 'Vložit do účtu', 'fio-transactions' ),
-			'uploaded_to_this_item' => __( 'Přiřazeno k tomuto účtu', 'fio-transactions' ),
-			'items_list'            => __( 'Seznam účtů', 'fio-transactions' ),
-			'items_list_navigation' => __( 'Navigace v seznamu účtů', 'fio-transactions' ),
-			'filter_items_list'     => __( 'Filtrovat účty', 'fio-transactions' )
+			'name'                  => _x( 'Správa účtů', 'Post Type General Name', 'fio-bank-transactions' ),
+			'singular_name'         => _x( 'Účet', 'Post Type Singular Name', 'fio-bank-transactions' ),
+			'menu_name'             => __( 'Správa účtů', 'fio-bank-transactions' ),
+			'name_admin_bar'        => __( 'Správa účtů', 'fio-bank-transactions' ),
+			'archives'              => __( 'Archiv účtů', 'fio-bank-transactions' ),
+			'attributes'            => __( 'Atributy', 'fio-bank-transactions' ),
+			'parent_item_colon'     => __( 'Nadřazený účet', 'fio-bank-transactions' ),
+			'all_items'             => __( 'Správa účtů', 'fio-bank-transactions' ),
+			'add_new_item'          => __( 'Přidat nový účet', 'fio-bank-transactions' ),
+			'add_new'               => __( 'Přidat účet', 'fio-bank-transactions' ),
+			'new_item'              => __( 'Nový účet', 'fio-bank-transactions' ),
+			'edit_item'             => __( 'Upravit účet', 'fio-bank-transactions' ),
+			'update_item'           => __( 'Aktualizovat účet', 'fio-bank-transactions' ),
+			'view_item'             => __( 'Zobrazit účet', 'fio-bank-transactions' ),
+			'view_items'            => __( 'Zobrazit účet', 'fio-bank-transactions' ),
+			'search_items'          => __( 'Hledat v účtech', 'fio-bank-transactions' ),
+			'not_found'             => __( 'Žádné účty', 'fio-bank-transactions' ),
+			'not_found_in_trash'    => __( 'Koš je prázdný', 'fio-bank-transactions' ),
+			'featured_image'        => __( 'Náhledový obrázek', 'fio-bank-transactions' ),
+			'set_featured_image'    => __( 'Zadat náhledový obrázek', 'fio-bank-transactions' ),
+			'remove_featured_image' => __( 'Odstranit náhledový obrázek', 'fio-bank-transactions' ),
+			'use_featured_image'    => __( 'Použít jako náhledový obrázek', 'fio-bank-transactions' ),
+			'insert_into_item'      => __( 'Vložit do účtu', 'fio-bank-transactions' ),
+			'uploaded_to_this_item' => __( 'Přiřazeno k tomuto účtu', 'fio-bank-transactions' ),
+			'items_list'            => __( 'Seznam účtů', 'fio-bank-transactions' ),
+			'items_list_navigation' => __( 'Navigace v seznamu účtů', 'fio-bank-transactions' ),
+			'filter_items_list'     => __( 'Filtrovat účty', 'fio-bank-transactions' )
 		];
 		$capabilities = [
 			'edit_post'              => Helpers::getFioManagerCapability(),
@@ -76,7 +76,7 @@ final class AccountsInit {
 			'create_posts'           => Helpers::getFioManagerCapability()
 		];
 		$args         = [
-			'label'               => __( 'Účty', 'fio-transactions' ),
+			'label'               => __( 'Účty', 'fio-bank-transactions' ),
 			'labels'              => $labels,
 			'supports'            => [ 'title', 'author', 'revisions' ],
 			'hierarchical'        => false,
@@ -109,7 +109,7 @@ final class AccountsInit {
 	public function titlePlaceholder( string $title ): string {
 		global $post_type;
 		if ( $post_type == self::ACCOUNTS_TYPE_SLUG ) {
-			$title = __( 'Zadejte název účtu', 'fio-transactions' );
+			$title = __( 'Zadejte název účtu', 'fio-bank-transactions' );
 		}
 
 		return $title;
@@ -119,20 +119,20 @@ final class AccountsInit {
 		$post                                 = get_post();
 		$messages[ self::ACCOUNTS_TYPE_SLUG ] = [
 			0  => '', // Unused. Messages start at index 1.
-			1  => __( 'Hotovo', 'fio-transactions' ), // My Post Type updated.
-			2  => __( 'Hotovo', 'fio-transactions' ), // Custom field updated.
-			3  => __( 'Hotovo', 'fio-transactions' ), // Custom field deleted.
-			4  => __( 'Hotovo', 'fio-transactions' ), // My Post Type updated.
-			5  => isset( $_GET['revision'] ) ? sprintf( __( 'Účet byl obnoven na starší verzi z %s' ), wp_post_revision_title( absint( $_GET['revision'] ), false ) ) : false,
-			6  => __( 'Hotovo', 'fio-transactions' ), // My Post Type published.
-			7  => __( 'Účet byl uložen', 'fio-transactions' ), // My Post Type saved.
-			8  => __( 'Hotovo', 'fio-transactions' ), // My Post Type submitted.
+			1  => __( 'Hotovo', 'fio-bank-transactions' ), // My Post Type updated.
+			2  => __( 'Hotovo', 'fio-bank-transactions' ), // Custom field updated.
+			3  => __( 'Hotovo', 'fio-bank-transactions' ), // Custom field deleted.
+			4  => __( 'Hotovo', 'fio-bank-transactions' ), // My Post Type updated.
+			5  => isset( $_GET['revision'] ) ? sprintf( __( 'Účet byl obnoven na starší verzi z %s'), wp_post_revision_title( absint( $_GET['revision'] ), false ) ) : false,
+			6  => __( 'Hotovo', 'fio-bank-transactions' ), // My Post Type published.
+			7  => __( 'Účet byl uložen', 'fio-bank-transactions' ), // My Post Type saved.
+			8  => __( 'Hotovo', 'fio-bank-transactions' ), // My Post Type submitted.
 			9  => sprintf(
 				__( 'Účet byl naplánován na: <strong>%1$s</strong>.' ),
 				// translators: Publish box date format, see http://php.net/date
 				date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) )
 			),
-			10 => __( 'Koncept účtu aktualizován', 'fio-transactions' ) // My Post Type draft updated.
+			10 => __( 'Koncept účtu aktualizován', 'fio-bank-transactions' ) // My Post Type draft updated.
 		];
 
 		return $messages;
