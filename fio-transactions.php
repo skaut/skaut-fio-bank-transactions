66a66c8665629567f7edc1a7e10a21dac9446f52
David Odehnal <info@davidodehnal.cz>
2017-09-12 11:38:28 +0200

    Update fio-transactions.php

--- fio-transactions.php
+++ fio-transactions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name:       FIO BANK - transactions
+ * Plugin Name:       Fio bank - transactions
  * Plugin URI:        https://github.com/skaut/fio-bank-transactions
  * Description:       Zobrazování transakcí z Fio banky.
  * Version:           0.1
