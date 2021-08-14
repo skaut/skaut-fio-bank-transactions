99af94d7eafdeec1844729de2c18438090f6129c
DAVE-PC\david <info@davidodehnal.cz>
2021-08-14 23:27:24 +0200

    fixes #31: Closure Notice - Security: Fio bank - transactions

--- plugin/src/Accounts/Columns.php
+++ plugin/src/Accounts/Columns.php
@@ -46,9 +46,9 @@ class Columns {
 		$modifiedDate   = sprintf( _x( 'Před %s', '%s = human-readable time difference', 'fio-bank-transactions' ), human_time_diff( strtotime( $post->post_modified ), current_time( 'timestamp' ) ) );
 		$modifiedAuthor = get_the_modified_author();
 
-		echo $modifiedDate;
+		echo esc_html( $modifiedDate );
 		echo '<br>';
-		echo '<strong>' . $modifiedAuthor . '</strong>';
+		echo '<strong>' . esc_html( $modifiedAuthor ) . '</strong>';
 
 	}
 
